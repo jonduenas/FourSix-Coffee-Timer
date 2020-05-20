f8865cc6cc0adc4d368f65a3f3172e3ab3058f1c
Jon Duenas <jonduenas@icloud.com>
2020-05-20 12:30:47 -0700

    Initial Commit

---
+++ FourSix Coffee Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSix Coffee Timer
+//
+//  Created by Jon Duenas on 5/20/20.
+//  Copyright © 2020 Jon Duenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
