542f96d1fabc88ea260565b379b95a5b2e651b4f
Jon Duenas <jonduenas@icloud.com>
2020-07-01 19:10:56 -0700

    Implement UIPickerView for selecting custom coffee and water amounts for recipe.

--- FourSix Coffee Timer/Controllers/BrewVC.swift
+++ FourSix Coffee Timer/Controllers/BrewVC.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class BrewVC: UIViewController {
+class BrewVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet var balanceSelect: UISegmentedControl!
     @IBOutlet var strengthSelect: UISegmentedControl!
+    
+    @IBOutlet var coffeeWaterPicker: UIPickerView!
     
     var calculator = Calculator()
     
@@ -20,12 +22,36 @@ class BrewVC: UIViewController {
     
     var showWalkthrough: Bool?
     
+    let coffeeWaterRatio = 15
+    var coffeeArray = [Int]()
+    var waterArray = [Int]()
+    var coffeeStringArray = [String]()
+    var waterStringArray = [String]()
+    var selectedCoffeeRow = 5
+    var selectedWaterRow = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.clearNavigationBar()
         
         loadUserDefaults()
+        
+        initializeCoffeeWaterPicker()
+    }
+    
+    fileprivate func initializeCoffeeWaterPicker() {
+        coffeeWaterPicker.delegate = self
+        coffeeWaterPicker.dataSource = self
+        
+        coffeeArray = Array(15...30)
+        waterArray = coffeeArray.map { $0 * coffeeWaterRatio }
+        
+        coffeeStringArray = coffeeArray.map(String.init)
+        waterStringArray = waterArray.map(String.init)
+        
+        coffeeWaterPicker.selectRow(selectedCoffeeRow, inComponent: 0, animated: false)
+        coffeeWaterPicker.selectRow(selectedWaterRow, inComponent: 1, animated: false)
     }
     
     //MARK: UI Methods
@@ -63,14 +89,39 @@ class BrewVC: UIViewController {
     
     @IBAction func calculateTapped(_ sender: Any) {
         
-        let totalWater: Double = 300
-        let coffee: Double = 20
+        let coffee = Double(coffeeArray[coffeeWaterPicker.selectedRow(inComponent: 0)])
+        let totalWater = Double(waterArray[coffeeWaterPicker.selectedRow(inComponent: 1)])
+        
         
         calculator.waterPours.removeAll()
         
         calculator.calculate(balance, strength, with: coffee, totalWater)
         
         saveUserDefaults()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coffeeStringArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "\(coffeeStringArray[row])g"
+        } else {
+            return "\(waterStringArray[row])g"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            pickerView.selectRow(row, inComponent: 1, animated: true)
+        } else {
+            pickerView.selectRow(row, inComponent: 0, animated: true)
+        }
     }
     
     //MARK: Navigation Methods
@@ -99,6 +150,8 @@ class BrewVC: UIViewController {
         defaults.set(balanceSelect.selectedSegmentIndex, forKey: "balanceSelect")
         defaults.set(strength.rawValue, forKey: "strength")
         defaults.set(strengthSelect.selectedSegmentIndex, forKey: "strengthSelect")
+        defaults.set(coffeeWaterPicker.selectedRow(inComponent: 0), forKey: "selectedCoffee")
+        defaults.set(coffeeWaterPicker.selectedRow(inComponent: 1), forKey: "selectedWater")
     }
     
     fileprivate func loadUserDefaults() {
@@ -115,6 +168,14 @@ class BrewVC: UIViewController {
         strength = Strength(rawValue: defaults.integer(forKey: "strength")) ?? .medium
         strengthSelect.selectedSegmentIndex = previousSelectedStrength
         print("\(strength)")
+        
+        if let previousSelectedCoffeeRow = defaults.object(forKey: "selectedCoffee") as? Int {
+            selectedCoffeeRow = previousSelectedCoffeeRow
+        }
+        
+        if let previousSelectedWaterRow = defaults.object(forKey: "selectedWater") as? Int {
+            selectedWaterRow = previousSelectedWaterRow
+        }
         
         //load Show Walkthrough option
         showWalkthrough = defaults.object(forKey: "walkthroughEnabled") as? Bool ?? true
