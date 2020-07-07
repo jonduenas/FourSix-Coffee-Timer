2d63ca316f51712a5ada0558f038f8ee108cf202
Jon Duenas <jonduenas@icloud.com>
2020-07-07 12:11:07 -0700

    Add CocoaPods open source acknowledgments to Settings Bundle. Add more acknowledgements to Settings in-app. Fix Settings storyboard error saying there was multiple color values for "Accent".

--- FourSix Coffee Timer/Controllers/AcknowledgementsVC.swift
+++ FourSix Coffee Timer/Controllers/AcknowledgementsVC.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class AcknowledgementsVC: UIViewController {
-
-    @IBOutlet var textView: UITextView!
+class AcknowledgementsVC: UITableViewController {
+    
+    @IBOutlet var ackLabel1: UILabel!
+    @IBOutlet var ackLabel2: UILabel!
+    @IBOutlet var ackLabel3: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textView.text = "4:6 Method invented by Tetsu Kasuya.\n\nTimer sound effect is \"Up Chime 2\" by FoolBoyMedia used under Creative Commons Attribution License 3.0."
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 240
+        
+        ackLabel1.text = "4:6 Method invented by Tetsu Kasuya."
+        ackLabel2.text = "Timer sound effect is \"Up Chime 2\" by FoolBoyMedia used under Creative Commons Attribution License 3.0."
+        ackLabel3.text = "TactileSlider Copyright (c) 2020 Dale Price <daprice@mac.com>, used under the MIT License, details of which can be found in System Settings."
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
