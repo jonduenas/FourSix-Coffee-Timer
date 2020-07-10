7a5828316c7a53d7c189cf27a4a3c1c3910acf5b
Jon Duenas <jonduenas@icloud.com>
2020-07-10 15:49:46 -0700

    Adjust some of the informational pages and add buttons at the bottom to easily move from one to the next.

--- FourSix Coffee Timer/Controllers/WhatIs46VC.swift
+++ FourSix Coffee Timer/Controllers/WhatIs46VC.swift
@@ -19,7 +19,7 @@ class WhatIs46VC: UIViewController {
 
         loadYoutube(videoID: "wmCW8xSWGZY")
         
-        contentLabel.text = "\"A new coffee brewing theory created with the concept of 'easily-brewed delicious coffee for everyone.' Divide water into a ratio of 4 to 6. Adjust taste with the first 40% of water and density with the remaining 60%.\n\n\"The point is to use coarse grounds and make the second pour after the water from the first pour completely drips through.\"*"
+        contentLabel.text = "FourSix is named after a method of brewing coffee called the \"4:6 method\" invented by Tetsu Kasuya, former World Brewer's Champion.\n\n4:6 is \"a new coffee brewing theory created with the concept of 'easily-brewed delicious coffee for everyone.' Divide water into a ratio of 4 to 6. Adjust taste with the first 40% of water and density with the remaining 60%.\n\n\"The point is to use coarse grounds and make the second pour after the water from the first pour completely drips through.\"*"
     }
     
     func loadYoutube(videoID:String) {
