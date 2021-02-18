ffb300e04facddb70a9a57753040d888a4c72ebb
Jon Duenas <jonduenas@icloud.com>
2021-02-18 13:10:36 -0800

    Use NumberFormatter for Clean extension so that returned String displays user's locale decimal separator.

--- FourSix Coffee Timer/Extensions/Clean.swift
+++ FourSix Coffee Timer/Extensions/Clean.swift
@@ -1,5 +1,5 @@
 //
-//  Double+clean.swift
+//  Clean.swift
 //  FourSix Coffee Timer
 //
 //  Created by Jon Duenas on 6/8/20.
@@ -11,12 +11,25 @@ import Foundation
 //Converts Double to String with decimal place if it has one or more and no decimal place if there are none
 extension Double {
     var clean: String {
-        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+        guard let formattedString = Formatter.decimal.string(for: self) else { return String(self) }
+        
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : formattedString
     }
 }
 
 extension Float {
     var clean: String {
-        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+        guard let formattedString = Formatter.decimal.string(for: self) else { return String(self) }
+        
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : formattedString
     }
+}
+
+extension Formatter {
+    static let decimal: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = .current
+        return numberFormatter
+    }()
 }
