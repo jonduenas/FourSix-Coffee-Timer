27d5de5d51f61211a4675199602bfb731254f94a
Jon Duenas <jonduenas@icloud.com>
2021-03-25 11:52:53 -0700

    Add entities and attributes in Core Data to match previous model structs.

--- FourSix Coffee Timer/Model/Note.swift
+++ FourSix Coffee Timer/Model/Note.swift
@@ -8,54 +8,54 @@
 
 import Foundation
 
-struct Note: Hashable {
-    let recipe: Recipe
-    let session: Session
-    let date: Date
-    var rating: Int
-    var noteText: String
-    var coffeeDetails: CoffeeDetails
-    var grindSetting: String
-    var waterTemp: Double
-    var waterTempUnit: UnitTemperature
-}
-
-struct Session: Hashable {
-    let averageDrawdown: TimeInterval
-    let totalTime: TimeInterval
-}
-
-struct CoffeeDetails: Hashable {
-    var roaster: String
-    var coffeeName: String
-    var origin: String
-    var roastDate: Date?
-    var roastLevel: String
-}
-
-extension Note {
-    static let testNote1 = Note(recipe: Recipe(coffee: 25, waterTotal: 350, waterPours: [50, 80, 120, 120], interval: 40, balance: .bright, strength: .strong),
-                                session: Session(averageDrawdown: 45, totalTime: 240),
-                                date: Date().addingTimeInterval(-108000),
-                                rating: 5,
-                                noteText: "This cup was perfect.",
-                                coffeeDetails: CoffeeDetails.testDetails1,
-                                grindSetting: "12",
-                                waterTemp: 100.0,
-                                waterTempUnit: .celsius)
-    static let testNote2 = Note(recipe: Recipe.defaultRecipe,
-                                session: Session(averageDrawdown: 47, totalTime: 485),
-                                date: Date().addingTimeInterval(-250568),
-                                rating: 3,
-                                noteText: "Grind finer.",
-                                coffeeDetails: CoffeeDetails.testDetails2,
-                                grindSetting: "18",
-                                waterTemp: 250.0,
-                                waterTempUnit: .fahrenheit)
-}
-
-extension CoffeeDetails {
-    static let testDetails1 = CoffeeDetails(roaster: "Coava", coffeeName: "Meaza", origin: "Ethiopia", roastDate: Date().addingTimeInterval(-965998), roastLevel: "Light")
-    
-    static let testDetails2 = CoffeeDetails(roaster: "Stumptown", coffeeName: "Hair Bender", origin: "Africa and Latin America", roastDate: Date(), roastLevel: "Medium")
-}
+//struct Note: Hashable {
+//    let recipe: Recipe
+//    let session: Session
+//    let date: Date
+//    var rating: Int
+//    var noteText: String
+//    var coffeeDetails: CoffeeDetails
+//    var grindSetting: String
+//    var waterTemp: Double
+//    var waterTempUnit: UnitTemperature
+//}
+//
+//struct Session: Hashable {
+//    let averageDrawdown: TimeInterval
+//    let totalTime: TimeInterval
+//}
+//
+//struct CoffeeDetails: Hashable {
+//    var roaster: String
+//    var coffeeName: String
+//    var origin: String
+//    var roastDate: Date?
+//    var roastLevel: String
+//}
+//
+//extension Note {
+//    static let testNote1 = Note(recipe: Recipe(coffee: 25, waterTotal: 350, waterPours: [50, 80, 120, 120], interval: 40, balance: .bright, strength: .strong),
+//                                session: Session(averageDrawdown: 45, totalTime: 240),
+//                                date: Date().addingTimeInterval(-108000),
+//                                rating: 5,
+//                                noteText: "This cup was perfect.",
+//                                coffeeDetails: CoffeeDetails.testDetails1,
+//                                grindSetting: "12",
+//                                waterTemp: 100.0,
+//                                waterTempUnit: .celsius)
+//    static let testNote2 = Note(recipe: Recipe.defaultRecipe,
+//                                session: Session(averageDrawdown: 47, totalTime: 485),
+//                                date: Date().addingTimeInterval(-250568),
+//                                rating: 3,
+//                                noteText: "Grind finer.",
+//                                coffeeDetails: CoffeeDetails.testDetails2,
+//                                grindSetting: "18",
+//                                waterTemp: 250.0,
+//                                waterTempUnit: .fahrenheit)
+//}
+//
+//extension CoffeeDetails {
+//    static let testDetails1 = CoffeeDetails(roaster: "Coava", coffeeName: "Meaza", origin: "Ethiopia", roastDate: Date().addingTimeInterval(-965998), roastLevel: "Light")
+//    
+//    static let testDetails2 = CoffeeDetails(roaster: "Stumptown", coffeeName: "Hair Bender", origin: "Africa and Latin America", roastDate: Date(), roastLevel: "Medium")
+//}
