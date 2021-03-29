25b0f8eb906868d42ce766fe22a44bf06e824dcd
Jon Duenas <jonduenas@icloud.com>
2021-03-29 16:59:30 -0700

    Refactor edit mode setting and add fix for background color not changing when switching system light and dark mode.

--- FourSix Coffee Timer/Controllers/Notes/NoteDetails/NotesTextView.swift
+++ FourSix Coffee Timer/Controllers/Notes/NoteDetails/NotesTextView.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class NotesTextView: UITextView {
-
+    private var editMode: Bool = false {
+        didSet {
+            layer.backgroundColor = editMode ? editOnColor.cgColor : editOffColor?.cgColor
+            isEditable = editMode
+        }
+    }
+    
+    private var editOnColor: UIColor = UIColor.systemBackground
+    private var editOffColor: UIColor? = UIColor(named: AssetsColor.background.rawValue)
+    
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         commonInit()
@@ -25,16 +34,23 @@ class NotesTextView: UITextView {
         layer.borderColor = UIColor.gray.withAlphaComponent(0.5).cgColor
         layer.borderWidth = 0.5
         clipsToBounds = true
-        setToEditMode(false)
+        layer.backgroundColor = editOffColor?.cgColor
+        isEditable = false
     }
     
     func setToEditMode(_ shouldSetToEdit: Bool) {
-        if shouldSetToEdit {
-            isEditable = true
-            layer.backgroundColor = UIColor.systemBackground.cgColor
-        } else {
-            isEditable = false
-            layer.backgroundColor = UIColor(named: AssetsColor.background.rawValue)?.cgColor
-        }
+        guard editMode != shouldSetToEdit else { return }
+        
+        editMode = shouldSetToEdit
+    }
+    
+    // Fix colors when switching between System Light and Dark Mode
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateColors()
+    }
+    
+    private func updateColors() {
+        layer.backgroundColor = editMode ? editOnColor.cgColor : editOffColor?.cgColor
     }
 }
