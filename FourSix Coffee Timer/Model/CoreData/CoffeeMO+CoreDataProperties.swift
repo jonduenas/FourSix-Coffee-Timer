4980897ba74afe118cfd7abe9b2076238aaa06bd
Jon Duenas <jonduenas@icloud.com>
2021-03-29 13:02:51 -0700

    Rename Coffee relationship to "notes" since it's a one to many relationship.

--- FourSix Coffee Timer/Model/CoreData/CoffeeMO+CoreDataProperties.swift
+++ FourSix Coffee Timer/Model/CoreData/CoffeeMO+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  CoffeeMO+CoreDataProperties.swift
 //  FourSix Coffee Timer
 //
-//  Created by Jon Duenas on 3/25/21.
+//  Created by Jon Duenas on 3/29/21.
 //  Copyright © 2021 Jon Duenas. All rights reserved.
 //
 //
@@ -17,29 +17,29 @@ extension CoffeeMO {
         return NSFetchRequest<CoffeeMO>(entityName: "Coffee")
     }
 
+    @NSManaged public var id: UUID
     @NSManaged public var name: String
     @NSManaged public var origin: String
     @NSManaged public var roaster: String
     @NSManaged public var roastLevel: String
-    @NSManaged public var id: UUID
-    @NSManaged public var note: NSSet?
+    @NSManaged public var notes: NSSet?
 
 }
 
-// MARK: Generated accessors for note
+// MARK: Generated accessors for notes
 extension CoffeeMO {
 
-    @objc(addNoteObject:)
-    @NSManaged public func addToNote(_ value: NoteMO)
+    @objc(addNotesObject:)
+    @NSManaged public func addToNotes(_ value: NoteMO)
 
-    @objc(removeNoteObject:)
-    @NSManaged public func removeFromNote(_ value: NoteMO)
+    @objc(removeNotesObject:)
+    @NSManaged public func removeFromNotes(_ value: NoteMO)
 
-    @objc(addNote:)
-    @NSManaged public func addToNote(_ values: NSSet)
+    @objc(addNotes:)
+    @NSManaged public func addToNotes(_ values: NSSet)
 
-    @objc(removeNote:)
-    @NSManaged public func removeFromNote(_ values: NSSet)
+    @objc(removeNotes:)
+    @NSManaged public func removeFromNotes(_ values: NSSet)
 
 }
 
