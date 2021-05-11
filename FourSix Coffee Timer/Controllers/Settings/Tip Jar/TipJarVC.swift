cc7ca49489f989aa832958c551e32e625d37f332
Jon Duenas <jonduenas@icloud.com>
2021-05-11 16:23:00 -0700

    Add UI for Tip Jar and add IBOutlet connections.

---
+++ FourSix Coffee Timer/Controllers/Settings/Tip Jar/TipJarVC.swift
@@ -0,0 +1,25 @@
+//
+//  TipJarVC.swift
+//  FourSix Coffee Timer
+//
+//  Created by Jon Duenas on 5/11/21.
+//  Copyright © 2021 Jon Duenas. All rights reserved.
+//
+
+import UIKit
+
+class TipJarVC: UIViewController {
+    
+    @IBOutlet weak var tip1View: UIView!
+    @IBOutlet weak var tip1PriceLabel: UILabel!
+    @IBOutlet weak var tip2View: UIView!
+    @IBOutlet weak var tip2PriceLabel: UILabel!
+    @IBOutlet weak var tip3View: UIView!
+    @IBOutlet weak var tip3PriceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
