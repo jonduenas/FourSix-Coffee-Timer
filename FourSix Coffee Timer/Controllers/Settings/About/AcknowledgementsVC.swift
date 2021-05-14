56e7702843cc2c76cec51e7276d1bd3a40c63745
Jon Duenas <jonduenas@icloud.com>
2021-05-14 11:11:34 -0700

    Remove TactileSlider from Acknowledgements.

--- FourSix Coffee Timer/Controllers/Settings/About/AcknowledgementsVC.swift
+++ FourSix Coffee Timer/Controllers/Settings/About/AcknowledgementsVC.swift
@@ -12,7 +12,6 @@ class AcknowledgementsVC: UITableViewController, Storyboarded {
     
     @IBOutlet var ackLabel1: UILabel!
     @IBOutlet var ackLabel2: UILabel!
-    @IBOutlet var ackLabel3: UILabel!
     @IBOutlet var ackLabel4: UILabel!
     
     override func viewDidLoad() {
@@ -23,7 +22,6 @@ class AcknowledgementsVC: UITableViewController, Storyboarded {
         
         ackLabel1.text = "4:6 Method invented by Tetsu Kasuya."
         ackLabel2.text = "Timer sound effect is \"Up Chime 2\" by FoolBoyMedia used under Creative Commons Attribution License 3.0."
-        ackLabel3.text = "TactileSlider Copyright (c) 2020 Dale Price <daprice@mac.com>, used under the MIT License, details of which can be found in System Settings > FourSix."
         ackLabel4.text = "Purchases Copyright (c) 2017 Jacob Eiting, used under the MIT License, details of which can be found in System Settings > FourSix."
     }
     
